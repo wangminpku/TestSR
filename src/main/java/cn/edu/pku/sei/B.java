3d8d493bb4366aea148784c8ca1232b9741cd398
wangmin <wangmin1994@pku.edu.cn>
2018-12-04 21:40:53 +0800

    first commit

---
+++ src/main/java/cn/edu/pku/sei/B.java
@@ -0,0 +1,11 @@
+package cn.edu.pku.sei;
+
+public abstract class B {
+
+    public int a;
+    public String s ;
+    public void m1(){
+        System.out.println("I am a tset method");
+    }
+
+}
---
+++ src/main/java/cn/edu/pku/sei/C.java
@@ -0,0 +1,11 @@
+package cn.edu.pku.sei;
+
+public class C extends B {
+
+    public int c;
+    public String c1;
+
+    public void m1(){
+        System.out.println("I am a test method for override");
+    }
+}
