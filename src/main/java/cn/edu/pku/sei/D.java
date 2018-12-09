4fbdfb8af4020f5353352bbde1e2f4f35f71cd11
wangmin <wangmin1994@pku.edu.cn>
2018-12-09 16:25:36 +0800

    change a var name in the same file ,test def-use in same file

--- src/main/java/cn/edu/pku/sei/D.java
+++ src/main/java/cn/edu/pku/sei/D.java
@@ -2,17 +2,17 @@ package cn.edu.pku.sei;
 
 public class D {
 
-    public String var1 = null;
+    public String var2 = null;
 
     public String getVar1() {
-        return var1;
+        return var2;
     }
 
     public void setVar1(String var1) {
-        this.var1 = var1;
+        this.var2 = var1;
     }
 
     public void UseVal(){
-        System.out.println(var1);
+        System.out.println(var2);
     }
 }
